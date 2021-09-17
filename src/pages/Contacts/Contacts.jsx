e7d5ea2c43ec023591eb0d85b4206f464ce8ec4c
Guilherme Santos <guiferrera@brq.com>
2021-09-17 18:06:02 -0300

    feat #08: Add header in contacts page

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,15 +1,50 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
+import { Grid } from "@material-ui/core";
+
+import filterIcon from "../../assets/icons/filter.png";
+import filterPlus from "../../assets/icons/plus.png";
+
+import {
+  Wrapper,
+  Header,
+  Search,
+  ContentsFilters,
+  FilterBtn,
+  AddBtn,
+} from "./styles";
 
 const Contacts = () => {
   return (
-    <section>
-      <h1>Contacts</h1>
+    <Wrapper>
+      <Header>
+        <Grid container alignItems="center" alignContent="center">
+          <h3>Contatos</h3>
+          <Search>
+            <input type="text" placeholder="Buscar..." />
+          </Search>
+        </Grid>
+        <ContentsFilters>
+          <FilterBtn>
+            <button>
+              <img src={filterIcon} alt="" />
+              <span>Filtrar</span>
+            </button>
+          </FilterBtn>
+          <AddBtn>
+            <button>
+              <img src={filterPlus} alt="" />
+              <span>Adicionar</span>
+            </button>
+          </AddBtn>
+        </ContentsFilters>
+      </Header>
 
       <Link to="/contatos-editar" className="button">
         edit contacts
       </Link>
-    </section>
+    </Wrapper>
   );
 };
 
---
+++ src/pages/Contacts/styles.js
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+
+import searchIcon from "../../assets/icons/search.png";
+
+export const Wrapper = styled.section`
+  position: relative;
+  width: 100%;
+  padding: 31px 24px;
+  background-color: #e5e5e5;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  align-items: center;
+
+  h3 {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 100%;
+    letter-spacing: -0.025em;
+  }
+`;
+
+export const Search = styled.div`
+  position: relative;
+
+  input[type="text"] {
+    background: #ffffff;
+    padding: 12px;
+    border: 1px solid #d1d1d1;
+    box-sizing: border-box;
+    border-radius: 6px;
+    min-width: 275px;
+    margin-left: 16px;
+  }
+
+  input[type="text"],
+  ::-webkit-input-placeholder {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 14px;
+    letter-spacing: -0.025em;
+    color: #a3a3a3;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    right: 0;
+    top: 0;
+    margin: 6px 9px;
+    background-image: url(${searchIcon});
+    background-repeat: no-repeat;
+    background-position: center;
+    z-index: 5;
+  }
+`;
+
+export const ContentsFilters = styled.div`
+  display: flex;
+
+  button {
+    font-family: "Inter";
+    display: flex;
+    align-content: center;
+    align-items: center;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 14px;
+    letter-spacing: -0.025em;
+    border-radius: 6px;
+    padding: 12px 16px;
+    height: 36px;
+    cursor: pointer;
+
+    span {
+      margin-left: 11px;
+    }
+  }
+`;
+
+export const FilterBtn = styled.div`
+  button {
+    color: #0065ff;
+    background: #ffffff;
+    border: 1px solid #cce8fe;
+    margin-right: 16px;
+  }
+`;
+
+export const AddBtn = styled.div`
+  button {
+    color: #ffffff;
+    background: #0065ff;
+    border: 1px solid #0065ff;
+  }
+`;
